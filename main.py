6cab003a2543c290b9cf110b2daf92a8a95b34cb
dania-ali <10dania.ali@gmail.com>
2025-01-17 04:56:33 +0000

    Prework: Added greeting, display menu w/placeholders, and user selection w/placeholders

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
-
-"""
-    Welcome to Elite 101 this program is a starter for your chatbot project.
-    The starter prompts the user to enter their name and then greets them with a personalized message.
-
-    Functions:
-        get_user_name(): Prompts the user to enter their name and returns it.
-        greet_user(name): Prints a greeting message using the provided name.
-        main(): Main function that orchestrates the user input and greeting process.
-
-    Execution:
-        When the script is run directly (not imported as a module), it will execute the main() function.
-"""
+#############
+#Section 1 - Import Modules and Global Variables
+#############
 
 
-def get_user_name():
-    return input("Please enter your name: ")
-
-def greet_user(name):
+#############
+#Section 2 - Functions Definition
+def greeting():
+    def get_user_name():
+        return input("Please enter your name: ")
+    def get_user_age():
+        return input("Please enter your age: ")
+    print("Welcome to the Elite 101 Chatbot!")
+    name = get_user_name()
     print(f"Hello, {name}!")
+    age = get_user_age()
+    print(f"Welcome {name}! Oh to be {age} again! How can I help you today?")D
 
-def main():
-    user_name = get_user_name()
-    greet_user(user_name)
 
-if __name__ == "__main__":
-    main()
+def display_menu():
+  print("\n **Please choose from the following options:**")
+  print("1. Placholder Option ")
+  print("2. Placholder Option ")
+  print("3. Placholder Option ")
+  print("4. Exit the conversation ")
+
+
+def user_selection():
+    #when prpgram starts it will be assigned to true, indicating the program is in use/running
+    in_use = True
+    user_input=int(input("Enter a number between 1-4:"))
+    if user_input == 1:
+        Placholder_Option()
+    elif user_input == 2:
+        Placholder_Option()
+    elif user_input == 3:
+        Placholder_Option()
+    elif user_input == 4:
+        print("Goodbye! Thank you for using the program!")
+        #this allows the user to exit the game as we're indicating that the program is not in use
+        in_use = False
+    else:
+        print("\nSorry, not a valid choice. Please try again. ")
+
+
+#############
+#Section 3 - Class Definition
+#############
+
+
+
+#############
+#Section 4 - Running Section
+greeting()
+display_menu()
+user_selection()
+#############
